8c3d5e56e33f10df933a1b8fd602dd9f203b10d0
jo030304 <j01040270138@gmail.com>
2025-04-10 00:26:57 +0900

    4주차 미션2

---
+++ chap4-mission2/4th-mission2/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router-dom";
+
+const HomePage = () => {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage;
---
+++ chap4-mission2/4th-mission2/src/pages/SignupPage.tsx
@@ -0,0 +1,9 @@
+const SignupPage = () => {
+  return (
+    <div>
+      SignUp Page
+    </div>
+  )
+}
+
+export default SignupPage;
